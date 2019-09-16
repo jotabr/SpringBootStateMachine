49e266b72b711d0c46f0ba66385a3c634abedc56
Jeison J duck <jeisonjd@br.ibm.com>
2019-09-16 14:37:52 -0300

    Initial commit.

---
+++ src/main/java/com/jjd/testesm/TesteSmApplication.java
@@ -0,0 +1,13 @@
+package com.jjd.testesm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteSmApplication.class, args);
+	}
+
+}
